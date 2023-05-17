780d1812d1ae788003413206df3dd8037b7d4840
kartiksharma2609 <kartik_sharma@webixun.com>
2023-05-17 16:56:41 +0530

    Test Answer file

---
+++ src/TeacherModule/Test/TestAnswers.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function TestAnswers() {
+  return (
+    <View>
+      <Text>TestAnswers</Text>
+    </View>
+  )
+}
+
+const style = StyleSheet.create({})
